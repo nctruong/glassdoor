d1d71080e02c4462ab245092a7e519e0c1bcd879
truong <willnguyen2971990@gmail.com>
2025-06-16 11:19:31 +0700

    deploy elasticsearch

--- client/services/authService.js
+++ client/services/authService.js
@@ -1,4 +1,11 @@
+import api from "./apiService.js";
+
 class AuthenticationService {
+
+    constructor(context) {
+        this.context = context;
+    }
+
     get token() {
         if (typeof window !== 'undefined') {
             return localStorage.getItem('token');
@@ -9,6 +16,12 @@ class AuthenticationService {
     get isLoggedIn() {
         return this.token !== null;
     }
+
+    // async get currentUser() {
+    //     const client = api(this.context);
+    //     const {data} = await client.get('/api/v1/users/currentUser');
+    //     return data.currentUser || null;
+    // };
 }
 
 export default new AuthenticationService();
