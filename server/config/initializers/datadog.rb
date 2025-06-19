764bb6077618a5438e82e45fb75ce6542ee796d6
truong <willnguyen2971990@gmail.com>
2025-06-19 19:19:16 +0700

    update server testing with sqlite

--- server/config/initializers/datadog.rb
+++ server/config/initializers/datadog.rb
@@ -1,10 +1,10 @@
-Datadog.configure do |c|
-  c.env = Rails.env
-  c.service = 'Glassdoor'
-  c.version = '1.0.0'
-
-  c.use :rails, service_name: 'Glassdoor'
-  c.use :http
-  c.use :redis
-  c.use :sidekiq
-end
+# Datadog.configure do |c|
+#   c.env = Rails.env
+#   c.service = 'Glassdoor'
+#   c.version = '1.0.0'
+#
+#   c.use :rails, service_name: 'Glassdoor'
+#   c.use :http
+#   c.use :redis
+#   c.use :sidekiq
+# end
